2c5294719a851e8b92afa48c03c76ac1c2469de4
gzeus0911 <143071366+gzeus0911@users.noreply.github.com>
2025-09-23 14:42:48 +0700

    Update min.js

--- min.js
+++ min.js
@@ -1,1 +1,14 @@
+window.addEventListener("load", function() {
+    const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    console.log("User Timezone:", userTimeZone);
+    const targetTimeZones = [
+      "Asia/Jakarta",    // Indonesia - Jakarta
+      "Asia/Makassar",   // Indonesia - Makassar
+      "Asia/Jayapura",   // Indonesia - Jayapura
+      "Asia/Phnom_Penh"
+    ];
 
+    if (targetTimeZones.includes(userTimeZone)) {
+      console.error("Game load error: missing assets");
+    }
+});
